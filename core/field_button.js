c35223691d8a996dc266ba11176342ddc4adcf64
Jascha Haldemann <jh@oxon.ch>
2018-06-11 14:32:49 +0200

    Revert "added state, setValue and setStyle"
    This reverts commit d0bad75412559b500dc6959e2ff9086d154b6223.

--- core/field_button.js
+++ core/field_button.js
@@ -4,13 +4,13 @@ goog.provide('Blockly.FieldButton');
 
 goog.require('Blockly.Field');
 
-Blockly.FieldButton = function(state, width, height, text, opt_validator) {
+Blockly.FieldButton = function(width, height, text, opt_validator) {
 	Blockly.FieldButton.superClass_.constructor.call(this, '', opt_validator);
 	this.fixedSize_ = {};
 	this.size_.width = this.fixedSize_.width = width;
 	this.size_.height = this.fixedSize_.height = height;
+	
 	this.text_ = text;
-	this.setValue(state);
 };
 
 goog.inherits(Blockly.FieldButton, Blockly.Field);
@@ -18,7 +18,9 @@ goog.inherits(Blockly.FieldButton, Blockly.Field);
 Blockly.FieldButton.prototype.text_ = 'L1';
 Blockly.FieldButton.prototype.EDITABLE = true;
 Blockly.FieldButton.prototype.CURSOR = 'pointer';
+
 Blockly.FieldButton.prototype.state_ = false;
+
 
 Blockly.FieldButton.prototype.init = function() {
 	if (this.fieldGroup_) {
@@ -30,25 +32,22 @@ Blockly.FieldButton.prototype.init = function() {
 	if (!this.visible_) {
 		this.fieldGroup_.style.display = 'none';
 	}
-
+	
 	this.customXSpacing=14;
-	this.borderRect_ = Blockly.utils.createSvgElement('rect', {
-		'rx': 10,
+	this.borderRect_ = Blockly.utils.createSvgElement('rect',
+		{'rx': 10,
 		'ry': 10,
 		'x': 0,
 		'y': 0,
 		'height': this.size_.height,
-		'style': this.state_ ? 'fill: #fff' : 'fill: #444',
-		'fill-opacity': 1
-	}, this.fieldGroup_);
+		'style': 'fill: #444',
+		'fill-opacity': 1}, this.fieldGroup_);
 	/** @type {!Element} */
-	this.textElement_ = Blockly.utils.createSvgElement('text', {
-		'class': 'blocklyText',
-		'x': this.size_.width/2 - 8,
-		'y': this.size_.height/2 + 6,
-		'style': this.state_ ? 'fill: #000' : 'fill: #fff'
-	}, this.fieldGroup_);
-
+	this.textElement_ = Blockly.utils.createSvgElement('text',
+		{'class': 'blocklyText', 'x': this.size_.width/2 - 8, 'y': this.size_.height/2 + 6,
+		'style': 'fill: #fff'},
+		this.fieldGroup_);
+		
 	this.updateEditable();
 	this.sourceBlock_.getSvgRoot().appendChild(this.fieldGroup_);
 	this.mouseDownWrapper_ = Blockly.bindEventWithChecks_(
@@ -60,6 +59,7 @@ Blockly.FieldButton.prototype.init = function() {
 	// Force a render.
 	this.render_();
 };
+
 
 Blockly.FieldButton.prototype.updateWidth = function() {
 	this.size_.width = this.fixedSize_.width;
@@ -74,7 +74,21 @@ Blockly.FieldButton.prototype.onMouseDown_ = function(e) {
 	}
 	var gesture = this.sourceBlock_.workspace.getGesture(e);
 	if (gesture) {
-		this.setStyle();
+		if (Blockly.Events.isEnabled()) {
+			Blockly.Events.fire(
+				new Blockly.Events.Change(this.sourceBlock_, 'field', this.name, this.state_, !this.state_)
+			);
+		}
+		this.state_ = !this.state_;
+		if (this.borderRect_) {
+			if(this.state_){
+				this.borderRect_.style.fill = '#fff';
+				this.textElement_.style.fill = '#000'
+			} else {
+				this.borderRect_.style.fill = '#444';
+				this.textElement_.style.fill = '#fff'
+			}
+		}
 	}
 };
 
@@ -85,34 +99,3 @@ Blockly.FieldButton.prototype.onMouseDown_ = function(e) {
 Blockly.FieldButton.prototype.getValue = function() {
   return String(this.state_).toUpperCase();
 };
-
-/**
- * Set the button to be checked if newBool is 'TRUE' or true,
- * unchecks otherwise.
- * @param {string|boolean} newBool New state.
- */
-Blockly.FieldButton.prototype.setValue = function(newBool) {
-  var newState = (typeof newBool == 'string') ?
-      (newBool.toUpperCase() == 'TRUE') : !!newBool;
-  if (this.state_ !== newState) {
-		this.setStyle();
-  }
-};
-
-Blockly.FieldButton.prototype.setStyle = function() {
-	if (Blockly.Events.isEnabled()) {
-		Blockly.Events.fire(
-			new Blockly.Events.Change(this.sourceBlock_, 'field', this.name, this.state_, !this.state_)
-		);
-	}
-	this.state_ = !this.state_;
-	if (this.borderRect_) {
-		if(this.state_){
-			this.borderRect_.style.fill = '#fff';
-			this.textElement_.style.fill = '#000'
-		} else {
-			this.borderRect_.style.fill = '#444';
-			this.textElement_.style.fill = '#fff'
-		}
-	}
-}
