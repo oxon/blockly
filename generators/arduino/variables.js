425a5f86802dcac773c48ec5be8bfbcc9569e2fc
Tim Luginbuehl <tl@oxon.ch>
2019-02-25 11:34:44 +0100

    examples compile again with variables prefixed

--- generators/arduino/variables.js
+++ generators/arduino/variables.js
@@ -19,8 +19,11 @@ goog.require('Blockly.Arduino');
  * @return {array} Completed code with order of operation.
  */
 Blockly.Arduino['variables_get'] = function(block) {
-	var code = Blockly.Arduino.variableDB_.getName(block.getFieldValue('VAR'),
-		Blockly.Variables.NAME_TYPE);
+	var code = Blockly.Arduino.CodeVariablesPrefix + 
+		Blockly.Arduino.variableDB_.getName(
+			block.getFieldValue('VAR'),
+			Blockly.Variables.NAME_TYPE
+		);
 	return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
@@ -34,8 +37,11 @@ Blockly.Arduino['variables_get'] = function(block) {
 Blockly.Arduino['variables_set'] = function(block) {
 	var argument0 = Blockly.Arduino.valueToCode(block, 'VALUE',
 		Blockly.Arduino.ORDER_ASSIGNMENT) || '0';
-	var varName = Blockly.Arduino.variableDB_.getName(
-		block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+	var varName = Blockly.Arduino.CodeVariablesPrefix +
+		Blockly.Arduino.variableDB_.getName(
+			block.getFieldValue('VAR'),
+			Blockly.Variables.NAME_TYPE
+		);
 	return varName + ' = ' + argument0 + ';\n';
 };
 
